4ba4ebc396786f87c9111c79173f183049c94526
JosefBertolini <39634656+JosefBertolini@users.noreply.github.com>
2020-08-02 19:27:30 -0700

    changed color scheme, made site more responsive

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+function copy_discord() {
+    const discord_name = "J03daSchm0#6160";
+
+    const temp = document.createElement('textarea');
+    temp.value = discord_name;
+    document.body.appendChild(temp);
+
+    temp.select();
+    document.execCommand("copy");
+    document.body.removeChild(temp);
+
+    var tooltip = document.getElementById("discord_tooltip");
+    tooltip.style.opacity = 1;
+    tooltip.style.visibility = "visible";
+}
+
+function mouse_exit_discord_button() {
+    var tooltip = document.getElementById("discord_tooltip");
+    tooltip.style.opacity = 0;
+    tooltip.style.visibility = "invisible";
+}
